7ac62fac4c8f4c50261c845980705f554a0c0576
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-25 15:23:03 +0200

    trait data processing

--- R/05_CPtrait.R
+++ R/05_CPtrait.R
@@ -1,48 +1,77 @@
 # Loading the data
 
-CPtrait <- read.csv("data-raw/05_CPtrait.csv", sep =';') %>%
-  select(-X.)
-
-Alltraits <- read.csv("data-raw/01_traits.csv")
-
 library(dplyr)
+library(assertr)
+library(readr)
+library(tidyr)
 
-try_join <- left_join(Alltraits, CPtrait) %>%
-  filter(!is.na(CPI1)) %>%
-  select(-species_id, -bwg_name, -starts_with('t'), -domain, -kingdom, -phylum, -subphylum,
-         -key, -functional_group, -predation, -realm, -micro_macro,
-         -barcode, -names) %>%
-  distinct()
+## read_csv2 uses ; as a field separator
+CPtrait <- read_csv2("data-raw/05_CPtrait.csv") %>%
+  select(-`#`)
 
-glimpse(try_join)
-
-setdiff(names(Alltraits), )
-setdiff(names(try_join), names(Alltraits))
-
-alltry_join <- left_join(Alltraits, try_join) %>%
-  filter(!is.na(CPI1)) %>%
-  glimpse()
-
-write.csv(alltry_join, "data-produced/alltry_join.csv")
-
-
-CP_join_bwgnames <- left_join(Alltraits, CPtrait, by = "bwg_name")
+Alltraits <- read_csv("data-raw/07_traits.csv")
 
 
 # Merge by bwgnames
+## drop misspelled column names, only need bwgcodes
 
 bwg_trait <- CPtrait %>%
-  select(bwg_name, starts_with("CP"))
+  select(bwg_name, starts_with("CP")) %>%
+  distinct() %>%
+  verify(nrow(.) == nrow(CPtrait))
 
-bwg_join <- left_join(Alltraits, bwg_trait, by = "bwg_name")
+bwg_join <- left_join(Alltraits, bwg_trait, by = "bwg_name") %>%
+  verify(nrow(.) == nrow(Alltraits))
+
+# Merge by species + genus
+
+CP_gen_spe <- CPtrait %>%
+  unite(gen_spe, genus, species) %>%
+  select(gen_spe, starts_with("CP")) %>%
+  distinct() %>%
+  filter(!grepl("NA_NA", gen_spe))
+
+CP_gen_spe %>%
+  filter(is.na(gen_spe)) %>%
+  verify(nrow(.) == 0)
+
+gen_spe_join <- bwg_join %>%
+  unite(gen_spe, genus, species) %>%
+  left_join(CP_gen_spe, by = "gen_spe")
+
+gen_spe_join %>% glimpse()
+bwg_join %>% glimpse()
+
+## there should be no double traits at this level
+CP_gen_spe %>%
+  group_by(gen_spe) %>%
+  tally %>%
+  filter(n > 1) %>%
+  semi_join(CP_gen_spe, .) %>%
+  verify(nrow(.) == 0)
+
+
+
+
+
+
+
+
 
 # Merge by genus
 
+
 CP_genus <- CPtrait %>%
   select(genus, starts_with("CP")) %>%
-  distinct()
+  distinct() %>%
+  filter(!is.na(genus))
+
+CP_genus %>%
+  filter(is.na(genus)) %>%
+  verify(nrow(.) == 0)
 
 genus_join <- left_join(bwg_join, CP_genus, by = "genus")
+glimpse(genus_join)
 
 genus_merged <- genus_join %>%
   mutate(CPG1 = ifelse(!is.na(CPI1.y), CPI1.y, CPI1.x)) %>%
@@ -72,11 +101,7 @@ CP_suborder <- CPtrait %>%
 
 order_join <- left_join(family_merged, CP_suborder, by = c("subord"))
 
-head(order_join)
-order_join %>%
-  mutate(eq = CPF1 == CPI1)
-  select(eq) %>% sum()
-
+ncol(family_merged)
 sum(order_join$CPF1 == order_join$CPI1, na.rm = TRUE)
 
 order_merged <- order_join %>%
@@ -119,3 +144,27 @@ CPtrait %>% select(bwg_name, CPI1) %>%
   filter(is.na(CPI1)) %>% glimpse()
 
 glimpse(family_merged)
+
+
+
+try_join <- left_join(Alltraits, CPtrait) %>%
+  filter(!is.na(CPI1)) %>%
+  select(-species_id, -bwg_name, -starts_with('t'), -domain, -kingdom, -phylum, -subphylum,
+         -key, -functional_group, -predation, -realm, -micro_macro,
+         -barcode, -names) %>%
+  distinct()
+
+glimpse(try_join)
+
+setdiff(names(Alltraits), )
+setdiff(names(try_join), names(Alltraits))
+
+alltry_join <- left_join(Alltraits, try_join) %>%
+  filter(!is.na(CPI1)) %>%
+  glimpse()
+
+write.csv(alltry_join, "data-produced/alltry_join.csv")
+
+
+
+CP_join_bwgnames <- left_join(Alltraits, CPtrait, by = "bwg_name")
