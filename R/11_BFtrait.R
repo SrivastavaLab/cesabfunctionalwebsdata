0b13210e8225dd81b07d4c8f9e924eab42ac4a0e
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-26 09:16:14 +0200

    extracting trait data

--- R/11_BFtrait.R
+++ R/11_BFtrait.R
@@ -5,18 +5,35 @@ library(assertr)
 library(readr)
 library(tidyr)
 library(daff)
+library(purrr)
 
 ## read_csv2 uses ; as a field separator
 
 Alltraits <- read_csv("data-raw/07_traits.csv")
 
+## remember to take out the NAS that are coced as characters
+
 # Create genera table
 
-genus <- Alltraits %>%
-  select(genus) %>%
-  filter(!is.na(genus)) %>%
-  distinct() %>%
-  arrange(genus) %>%  as.data.frame()
+unique_taxa <- function(taxon){
+  Alltraits %>%
+    select_(taxon) %>%
+    {.[!is.na(.[[taxon]]), ]} %>%
+    {.[.[[taxon]] != "NA", ]} %>%
+    # filter(!is.na(genus)) %>% ## complete cases; filter NA s that are words
+    distinct() %>%
+    arrange()
+}
+
+unique_taxa("genus")
+unique_taxa("family")
+
+taxon_list <- names(Alltraits)[3:14] %>%
+  set_names() %>%
+  map(unique_taxa)
+
+### HERE ANDREW
+for i
 
 familyt <- Alltraits %>%
   select(family) %>%
