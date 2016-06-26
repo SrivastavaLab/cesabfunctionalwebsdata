bc1adef539e6e016c8eb6fa31a3fd109d1ca4edf
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-26 09:14:27 +0200

    minor changes (mostly formatting)

--- R/02_summarizing_data.R
+++ R/02_summarizing_data.R
@@ -8,41 +8,55 @@ library(stringr)
 ## convenience function for reading
 source("R/reading_functions.R")
 
-# bromeliads imporing with correct col types--------------------------------------------------------------
+# bromeliads:  imporing with correct col types--------------------------------------------------------------
 
 broms <- read_csv_correct_cols("data-raw/01_broms.csv")
+visits <- read_csv("data-raw/01_visits.csv", col_types = "nncDnnnnnncnncc")
+datasets<- read_csv("data-raw/01_datasets.csv")
 
 ## the "problems" are probaby OK?
 problems(broms)
 broms$ph
 ## yes --- they look just fine
-
-##making some summary tables to help with visualizing missing data
-
-visits <- read_csv("data-raw/01_visits.csv", col_types = "nncDnnnnnncnncc")
 glimpse(visits)
+
+## making some summary tables to help with visualizing missing data -----------------------------------------
+
 
 visitnames <- visits %>%
   select(visit_id, dataset_id, dataset_name)
-datasetnames <- select(visitnames, dataset_id, dataset_name)%>%
-  group_by(dataset_id)%>%summarise(dataset_name=first(dataset_name))
 
-datasets<- read_csv("data-raw/01_datasets.csv")
+datasetnames <- visitnames %>%
+  select(dataset_id, dataset_name) %>%
+  distinct()
+
+
 names(datasets)
 
-vol_table<-broms%>%group_by(visit_id)%>%summarise(max_water=mean(max_water,na.rm=TRUE), species=first(species),extended_diameter=mean(extended_diameter,na.rm=TRUE), diameter=mean(diameter, na.rm=TRUE),
-                                                  longest_leaf=mean(longest_leaf, na.rm=TRUE), num_leaf=mean(num_leaf, na.rm=TRUE), leaf_width=mean(leaf_width, na.rm=TRUE), plant_height_cm=mean(plant_height_cm, na.rm=TRUE))%>%
+## generate bromeliad-level data for each visit and each species
+
+vol_table <- broms %>%
+  group_by(visit_id, species) %>%
+  summarise(max_water         = mean(max_water,na.rm=TRUE),
+            extended_diameter = mean(extended_diameter,na.rm=TRUE),
+            diameter          = mean(diameter, na.rm=TRUE),
+            longest_leaf      = mean(longest_leaf, na.rm=TRUE),
+            num_leaf          = mean(num_leaf, na.rm=TRUE),
+            leaf_width        = mean(leaf_width, na.rm=TRUE),
+            plant_height_cm   = mean(plant_height_cm, na.rm=TRUE)) %>%
   left_join(visitnames)
 
+## For each bromeliad select the diameter
 diam_brom <- broms %>%
   select(-min, -max, -mass) %>%
   distinct %>%
-  select(diameter, bromeliad_id)
+  select(bromeliad_id, diameter)
 
+## for each bromeliad select the fine detrius
 fpom_brom <- broms %>%
   select(-min, -max, -mass) %>%
   distinct %>%
-  select(fpom_ml,fpom_g,cpom_g,dead_leaves, bromeliad_id)
+  select(bromeliad_id, fpom_ml, fpom_g, cpom_g, dead_leaves)
 
 
 ## here we make the different detritus categories into a wide format, one column
@@ -56,28 +70,37 @@ detritus_wide <- broms %>%
 ## combine back with the visit ids
 detritus_wider <- broms %>%
   select(visit_id, bromeliad_id) %>%
-  group_by(bromeliad_id) %>%
-  summarize(visit_id = first(visit_id)) %>%
+  distinct %>%
+  # group_by(bromeliad_id) %>%
+  # summarize(visit_id = first(visit_id)) %>%
   left_join(detritus_wide)%>%
   left_join(visitnames)%>%
   left_join(diam_brom)%>%
   left_join(fpom_brom)
 
+## create for references
+detritus_original <- detritus_wider
+
 #collapse to dataset level for easy checking
 detrital_table <- detritus_wider %>%
-  select(-bromeliad_id, -dataset_name) %>%
+  select(-bromeliad_id, -dataset_name, dataset_id) %>%
   group_by(visit_id) %>%
   summarise_each(funs(mean(., na.rm = TRUE)))%>%
   left_join(datasetnames)
 
 ### predicting the fine detritus from the rest of the data####################
 
+## should be no copying over
+where_values <- function(x) which(!is.na(x))
+
+
 ## for cardoso 2008
 fine_cardoso2008<- function(coarse){
   exp(0.68961 * log(coarse) - 0.11363)
 }
 
-detritus_wider<-detritus_wider %>%
+
+detritus_wider <- detritus_wider %>%
   mutate(detritus0_150 = ifelse(visit_id == 21, fine_cardoso2008(detritus150_20000), detritus0_150))
 
 
@@ -92,8 +115,10 @@ detritus_wider<-detritus_wider %>%
 ## Although Puerto Rico 2010 dataset=116 based only on relaxed diameter the adj r sq is 0.79
 #eqn from all 1990s El verde plants (n=189, rsq = 0.78) interestingly v. similar eqn from pitilla 2002 secondary
 
-elverde90s<-detritus_wider%>%filter(dataset_id==131| dataset_id==126|dataset_id==121|dataset_id==221)
-elverde90s$detritus0_NA<-(elverde90s$detritus10_1500+elverde90s$detritus1500_20000+elverde90s$detritus20000_NA)
+elverde90s <- detritus_wider %>%
+  filter(dataset_id==131| dataset_id==126|dataset_id==121|dataset_id==221)
+
+elverde90s$detritus0_NA <- with(elverde90s, detritus10_1500 + detritus1500_20000 + detritus20000_NA)
 summary(glm(log(detritus0_NA)~log(diameter), data=elverde90s))
 plot(log(elverde90s$detritus0_NA)~log(elverde90s$diameter))
 
@@ -202,33 +227,19 @@ detritus_wider<-detritus_wider%>%
   mutate(detritus10000_NA = ifelse(dataset_id%in%c(101,106), deadleavesalso_pitilla(detritus22_10000), NA))
 
 
-### this script summarizes detritus amounts -- run it after you have imputed missing values
-det <- broms %>%
-  select(bromeliad_id, min, max, mass) %>%
-  # semi_join(manys %>% filter(n > 4)) ## just to check with the eye.
-  group_by(bromeliad_id) %>%
-  summarise(detritus_mass_total = sum(mass, na.rm = TRUE))
+daff::render_diff(daff::diff_data(detritus_original, detritus_wider))
 
-summ_brom <- broms %>%
-  select(-min, -max, -mass) %>%
-  distinct %>%
-  left_join(det)
-
-
-write_csv(summ_brom, "data-raw/02_broms.csv")
-
-
-
-# abundance ---------------------------------------------------------------
-
-abund <- read_csv("data-raw/01_abundance.csv")
-
-glimpse(abund)
-
-summ_abund <- abund %>%
-  select(-category_range, -measurement) %>%
-  group_by(dataset_id, species_id, bwg_name, brm) %>%
-  summarise(abundance = sum(abd, na.rm = TRUE))
-
-write_csv(summ_abund, "data-raw/02_abundance.csv")
-
+# ### this script summarizes detritus amounts -- run it after you have imputed missing values
+# det <- broms %>%
+#   select(bromeliad_id, min, max, mass) %>%
+#   # semi_join(manys %>% filter(n > 4)) ## just to check with the eye.
+#   group_by(bromeliad_id) %>%
+#   summarise(detritus_mass_total = sum(mass, na.rm = TRUE))
+#
+# summ_brom <- broms %>%
+#   select(-min, -max, -mass) %>%
+#   distinct %>%
+#   left_join(det)
+#
+#
+# write_csv(summ_brom, "data-raw/02_broms.csv")
