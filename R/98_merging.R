d448c6039b61e21f19818fdb5a73f81d316a5a77
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-27 14:31:13 +0200

    rename the merging script

--- R/98_merging.R
+++ R/98_merging.R
@@ -7,8 +7,8 @@
 require(readr)
 datasets <- read_csv("data-raw/01_datasets.csv")
 visits <-   read_csv("data-raw/01_visits.csv")
-traits <-   read_csv("data-raw/01_traits.csv")
-bromeliads <- read_csv("data-raw/02_broms.csv")
+traits <-   read_csv("data-raw/07_traits.csv")
+bromeliads <- read_csv("data-raw/61_bromeliad_wide.csv")
 abundance <- read_csv("data-raw/02_abundance.csv")
 
 all_data_list <- list(
