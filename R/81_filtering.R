70595e570cad32a8fea92048a7a790ec32faf7d7
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-27 13:23:51 +0200

    functions to filter data

--- R/81_filtering.R
+++ R/81_filtering.R
@@ -10,7 +10,7 @@ Ematrix <- read.csv("data-raw/41_E_matrix.csv")
 # Filter out NAs
 
 any_NA <- function(vector){
-  any(is.na(vector))
+  any(any(is.na(vector)), sum(vector) == 0)
 }
 
 all_NA <- function(vector){
@@ -27,20 +27,20 @@ E_all_NA <- Ematrix %>%
 E_complete <- Ematrix %>%
   .[!E_all_NA, ]
 
-## is detritus negative.
+Brom_names_det <- Ematrix %>%
+  filter(!is.na(detritus_sum)) %>%
+  select(bromeliad_id) %>%
+  .[['bromeliad_id']]
 
-sum(E_complete$detritus_sum < 0, na.rm = TRUE)
+Brom_names_vol <- Ematrix %>%
+  filter(!is.na(max_water)) %>%
+  select(bromeliad_id) %>%
+  .[['bromeliad_id']]
 
-
-names(E_all_NA) <-
-
-E_final <- E_all_NA %>%
-  filter(!all_NA) %>%
-  select(-all_NA)
-
-
-
-
+Brom_names_vol_det <- Ematrix %>%
+  filter(complete.cases(.)) %>%
+  select(bromeliad_id) %>%
+  .[['bromeliad_id']]
 
 ### Trait filtering
 
@@ -49,16 +49,28 @@ E_final <- E_all_NA %>%
 T_aquatic <- Tmatrix %>%
   filter(realm != "terrestrial", micro_macro != "micro")
 
+T_r_all_NA <- Tmatrix %>%
+  select(matches("^[A-Z]{2}\\d")) %>%
+  apply(1, all_NA)
 
+## symm for report
 
+T_r_no_na <- Tmatrix %>%
+  .[!T_r_all_NA, ]
 
+T_c_all_NA <- T_r_no_na %>%
+  select(matches("^[A-Z]{2}\\d")) %>%
+  apply(2, all_NA)
 
+T_complete <- T_r_no_na %>%
+  .[!T_c_all_NA, ]
 
-Wmatrix %>%
+## W filtering
+
+Wmatrix %>% glimpse()
   apply(1, sum) %>%
 
-  apply(1, filter_if_all_NA) %>%
-  cbind(Wmatrix) %>%
+
 
 
 
