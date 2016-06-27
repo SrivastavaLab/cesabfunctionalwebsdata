b063a14a8054f9b20625d7aa6fd46dfc4a3cab0f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-27 14:31:57 +0200

    Filtering WBE matrices & associated functions

--- R/81_filtering.R
+++ R/81_filtering.R
@@ -6,41 +6,28 @@ Tmatrix <- read.csv("data-raw/13_T_matrix.csv")
 
 Ematrix <- read.csv("data-raw/41_E_matrix.csv")
 
-
-# Filter out NAs
-
-any_NA <- function(vector){
-  any(any(is.na(vector)), sum(vector) == 0)
-}
-
-all_NA <- function(vector){
-  all(is.na(vector))
-}
+source("R/output_cleaning_functions.R")
 
 
-### Environmental filtering
 
-E_all_NA <- Ematrix %>%
-  select(max_water, detritus_sum) %>%
-  apply(1, all_NA)
+# Filtering Rows and columns -- Ematrix -----------------------------------
 
-E_complete <- Ematrix %>%
-  .[!E_all_NA, ]
+Ematrix_row_col <- Ematrix %>%
+  drop_row_col(c("detritus_sum", "max_water"), all_NA)
 
-Brom_names_det <- Ematrix %>%
-  filter(!is.na(detritus_sum)) %>%
-  select(bromeliad_id) %>%
-  .[['bromeliad_id']]
+# E matrix -- List acceptable bromelid_ids --------------------------------
 
-Brom_names_vol <- Ematrix %>%
-  filter(!is.na(max_water)) %>%
-  select(bromeliad_id) %>%
-  .[['bromeliad_id']]
 
-Brom_names_vol_det <- Ematrix %>%
+brom_names_list <- c("detritus_sum", "max_water") %>%
+  set_names() %>%
+  map(get_bromid_nonNA, data_matrix = ematrix_row_col)
+
+
+Brom_names_vol_det <- ematrix_row_col %>%
   filter(complete.cases(.)) %>%
-  select(bromeliad_id) %>%
   .[['bromeliad_id']]
+
+output_list <- splice(brom_names_list, complete = Brom_names_vol_det)
 
 ### Trait filtering
 
@@ -49,31 +36,26 @@ Brom_names_vol_det <- Ematrix %>%
 T_aquatic <- Tmatrix %>%
   filter(realm != "terrestrial", micro_macro != "micro")
 
-T_r_all_NA <- Tmatrix %>%
-  select(matches("^[A-Z]{2}\\d")) %>%
-  apply(1, all_NA)
+trait_names <- names(T_aquatic)[grepl("^[A-Z]{2}\\d", names(T_aquatic))]
+Tmatrix_row_col <- T_aquatic %>%
+  drop_row_col(trait_names, any_NA_or_0)
 
 ## symm for report
 
-T_r_no_na <- Tmatrix %>%
-  .[!T_r_all_NA, ]
 
-T_c_all_NA <- T_r_no_na %>%
-  select(matches("^[A-Z]{2}\\d")) %>%
-  apply(2, all_NA)
+## W filtering ----------------------
 
-T_complete <- T_r_no_na %>%
-  .[!T_c_all_NA, ]
-
-## W filtering
-
-Wmatrix %>% glimpse()
-  apply(1, sum) %>%
+brom_names <- names(Wmatrix)[grepl("^X", names(Wmatrix))]
+Wmatrix_row_col <- Wmatrix %>%
+  drop_row_col(brom_names, any_NA_or_0)
 
 
+full_list <- list(E = Ematrix_row_col,
+     W = Wmatrix_row_col,
+     B = Tmatrix_row_col,
+     filter_environments = output_list)
 
-
-
+saveRDS(full_list, "data-raw/81_WBE.RDS")
 
 
 
