aa9c29f1ef46896381068934ab71529f7cec1143
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-27 17:04:30 +0200

    create more accurate W matrix

--- R/51_abundance_W_matrix.R
+++ R/51_abundance_W_matrix.R
@@ -11,11 +11,18 @@ abunds <- read.csv("data-raw/01_abundance.csv")
 
 # summarize  --------------------------------------------------------------
 ### nope not yet!!
-wide_spp <- abunds %>%
+summed_abundance_spp <- abunds %>%
   group_by(dataset_id, species_id, bwg_name, brm) %>%
   summarise(abd = sum(abd, na.rm = TRUE)) %>%
+  ungroup
+
+write_csv(summed_abundance_spp, "data-raw/51_abundance.csv")
+
+wide_spp <- summed_abundance_spp %>%
   select(-bwg_name) %>%
-  distinct %>%
-  spread(species_id, abd)
+  unite("dataset_species", dataset_id, species_id) %>%
+  spread(dataset_species, abd, fill = 0)
+
+dim(wide_spp)
 
 write_csv(wide_spp, "data-raw/51_W_matrix.csv")
