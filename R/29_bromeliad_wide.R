d72d14f5ede75c77c012a5f7bae8cb197ec28c13
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-06-28 07:15:43 +0200

    use the widest bromeliad data as base for this merge

--- R/29_bromeliad_wide.R
+++ R/29_bromeliad_wide.R
@@ -1,17 +1,17 @@
 library(dplyr)
 
-bromeliad_wide <- read.csv("data-raw/02_broms.csv")
-detritus_final <- read.csv("data-raw/22_detritus_summary.csv")
-volume_final <- read.csv("data-raw/24_volume.csv")
-open_final <- read.csv("data-raw/23_open.csv")
+bromeliad_wide <- read.csv("data-raw/02_bromeliad_wide.csv", stringsAsFactors = FALSE)
+detritus_final <- read.csv("data-raw/22_detritus_summary.csv", stringsAsFactors = FALSE)
+volume_final <- read.csv("data-raw/24_volume.csv", stringsAsFactors = FALSE)
+open_final <- read.csv("data-raw/23_open.csv", stringsAsFactors = FALSE)
 
-brom_vol <- left_join(bromeliad_wide, volume_final)
+brom_vol <- left_join(bromeliad_wide, volume_final, by = c("bromeliad_id"))
 
 det <- detritus_final %>%
   select(-detritus_range)
 
-brom_det_vol <- left_join(brom_vol, det)
+brom_det_vol <- left_join(brom_vol, det, by = c("bromeliad_id"))
 
-brom_det_vol_open <- left_join(brom_det_vol, open_final)
+brom_det_vol_open <- left_join(brom_det_vol, open_final, by = c("bromeliad_id"))
 
-write_csv(emat, "data-raw/29_full_bromeliad_wide.csv")
+write_csv(brom_det_vol_open, "data-raw/29_full_bromeliad_wide.csv")
