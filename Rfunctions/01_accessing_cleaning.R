537a1ee5d33bb0156f88a0ec7c4b32b4520b3455
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-09-12 17:19:55 +0200

    comments, and edits to remake script

--- Rfunctions/01_accessing_cleaning.R
+++ Rfunctions/01_accessing_cleaning.R
@@ -1,18 +1,45 @@
+# accessing -----------------------------------------------------
 
-fiter_test <- function(x){
+## Getting the abundance matrix for each site requires querying its precise dataset ID
+## this convenience function creates a named vector of all the datasets and downloads them all
+## can be slow
+
+get_all_abundances <- function(.dats){
+  abds_ <- .dats %>%
+    .[["dataset_id"]] %>%
+    as.numeric %>%
+    set_names %>%
+    map(~ list(dataset_id = .x)) %>%
+    map(~ bwg_get("matrix", .))
+
+  return(abds_)
+}
+
+
+
+# Filtering ---------------------------------------------------------------
+
+## NOTE eventualy these will be deleted and then this test will be deleted
+## because it will be useless
+filter_test <- function(x){
   x %>%
     filter(!grepl("^Test", name))
 }
 
 
+# Restructuring -------------------------------------------------------------
+
+
 no_attrib_unnest_det <- function(.broms){
+
+  ## Drop the attributes part of the variable names
   names(.broms) <- str_replace(names(.broms), "attributes\\.", "")
+  ## yes this makes the function a bit not modular. If you're reading this you
+  ## can feel quite free to judge :)
 
-  ### the detritus should be unnested (for raw. summarize later)
-
-
+  ### the detritus should be unnested (for raw. summarize later, across columns)
   .broms %>%
-    ## supply a default data.frame
+    ## supply a default data.frame, or otherwise unnesting chokes
     mutate(detritus = map_if(detritus,
                              is_null,
                              ~ data_frame(min = NA,
@@ -23,12 +50,15 @@ no_attrib_unnest_det <- function(.broms){
 
 combine_multi_names <- function(.trts_all){
   ### names are a nested list. Convert them to a vector of length one.
+  ## bespoke predicate function to detect multiple names
   is_long <- function(x) length(x) > 1
 
   .trts_all %>%
     mutate(names = names %>%
+             ## again, supply a sensible default (an empty word, not a NULL)
              map_if(is_null, ~"") %>%
              map_if(is_long, paste, collapse = ";")) %>%
+    ## unnesst safely, since now it is a wholesome list of length 1 dataframes
     unnest(names)
 
 }
