fba9b508480d1b6bc782c61158d141b206665427
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-11-07 15:23:10 +0100

    working out my little model prediction demonstration

--- R/example.R
+++ R/example.R
@@ -1,12 +1,12 @@
 
 dd <- data_frame(x = runif(15, 2, 20),
-                 y = x * 3 + 15 + rnorm(n = 15, sd = 5))
+                 y = x * 3 + 15 + rnorm(n = 15, sd = 9))
 
 p <- dd %>%
-  ggplot(aes(x = x, y = y)) + geom_point()
-
+  ggplot(aes(x = x, y = y)) + geom_point() + stat_smooth(method = "lm")
+p
 mm <- lm(y ~ x, data = dd)
-
+summary(mm)
 predict_function <- function(mod) {
   force(mod)
   function(xs) {
@@ -17,9 +17,13 @@ newx <- runif(20, 3, 50)
 new <- predict_function(mm)(newx)
 
 
-p + geom_pointrange(aes(ymin = ymin,
+pe <- p + geom_pointrange(aes(ymin = ymin,
                         ymax = ymax),
                     data = data_frame(x = newx,
                                       y = new$fit,
                                       ymin = new$fit - new$se.fit,
                                       ymax = new$fit + new$se.fit), colour = "red")
+pe + coord_cartesian(xlim = c(0, 25), ylim = c(0, 100))
+
+pe
+
