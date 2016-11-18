37f8145e431dd135e26960b4c0474903f215bb92
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-11-18 08:33:15 +0100

    model, test, and display predictions

--- Rfunctions/98_function_against_data.R
+++ Rfunctions/98_function_against_data.R
@@ -53,7 +53,7 @@ detritus_wider_correct_frenchguiana %>%
 estimating_function_data <-
   frame_data(
     ~target_dat, ~src_dat,                       ~eqn,                                                              ~xvar,               ~yvar,
-    116,         c("131", "126", "121", "221"),  list(~ detritus10_1500 + detritus1500_20000 + detritus20000_NA),  list(~detritus0_NA),  list(~log(detritus0_NA))
+    116,         c("131", "126", "121", "221"),  list(~ detritus10_1500 + detritus1500_20000 + detritus20000_NA),  list(~diameter),  list(~log(detritus0_NA))
   )
 
 test <- estimating_function_data %>%
@@ -67,7 +67,50 @@ test <- estimating_function_data %>%
          mod = map2(.x = src_newv, .y = fml, ~ fit_with(data = .x, .formulas = .y, .f = glm))
   )
 
+## for validating
+test %>%
+  select(src_newv, fml, mod)
 
-detritus_wider_correct_frenchguiana %>% filter(dataset_id %in% c("131", "126"))
+test$src_newv[[1]] %>%
+  add_predictions(model = test$mod[[1]][[1]], var = "detritus0_NA_pred") %>%
+  mutate(detritus0_NA_pred = exp(detritus0_NA_pred)) %>%
+  ggplot(aes(x = diameter, y = detritus0_NA)) +
+  geom_point() +
+  geom_line(aes(y = detritus0_NA_pred)) +
+  coord_trans(y = "log")
 
-estimating_function_data$src_dat
+rmse(test$mod[[1]][[1]], test$src_newv[[1]])
+rsquare(test$mod[[1]][[1]], test$src_newv[[1]])
+mae(test$mod[[1]][[1]], test$src_newv[[1]])
+
+list(rmse, rsquare, mae) %>%
+  invoke_map(model = test$mod[[1]][[1]], data = test$src_newv[[1]])
+
+# split by target or source data, predict (use same name) then combine -- labels
+# to new column called something like obs_or_prediction
+
+df_list <- test %>%
+  select(target_dat, src_newv, mod) %>%
+  # get the targetd df just as above
+  mutate(target_df = map(target_dat, ~ detritus_wider_correct_frenchguiana %>%
+                        filter(dataset_id %in% .x)),
+         # add predictions to it
+         target_df_pred = map2(target_df, mod, ~ add_predictions(.x, .y[[1]], "detritus0_NA")),
+         target_df_pred = map(target_df_pred, ~ .x %>% mutate(detritus0_NA = exp(detritus0_NA)))
+  ) %>%
+  {list(observed = .[["src_newv"]],
+        predicted = .[["target_df_pred"]]
+        )}
+
+df_list %>%
+  flatten %>%
+  bind_rows(.id = "detritus0_NA_pred") %>%
+  ggplot(aes(x = diameter, y = detritus0_NA, colour = detritus0_NA_pred)) +
+  geom_point() +
+  coord_trans(y = "log")
+
+estimating_function_data %>%
+  mutate()
+filter(, estimating_function_data)
+
+## maybe one huge data.frame is not helpful -- try invoke_rows with smaller, function-specific data_frames that can then be gathered
