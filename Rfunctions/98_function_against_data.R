a9825072ff7249f72258603652c7a75cfaf403ee
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-11-20 23:24:25 +0100

    comment out stuff that doesn't help

--- Rfunctions/98_function_against_data.R
+++ Rfunctions/98_function_against_data.R
@@ -75,40 +75,42 @@ fit_to_real_life <- estimate_missing_detritus_new_site(.observed_model_fit = obs
                                                        .detritus_data = detritus_wider_FG_detritus_corrected)
 
 
+fit_to_real_life %>% map(select, 36) %>% map(head)
 
 
-## for validating
-
-list(rmse, rsquare, mae) %>%
-  map(invoke_rows, .d = test_mod %>% select(m_id, data = src_df, model = predicting_model), .collate = "cols")
-# TODO extract these into a data_frame
-
-list(rmse, rsquare, mae) %>%
-  invoke_map(list(model = test_mod$predicting_model,
-                  data = test_mod$src_df))
-
-# split by target or source data, predict (use same name) then combine -- labels
-# to new column called something like obs_or_prediction
-
-df_list <- test %>%
-  select(target_dat, src_newv, mod) %>%
-  # get the targetd df just as above
-  mutate(target_df = map(target_dat, ~ detritus_wider_correct_frenchguiana %>%
-                        filter(dataset_id %in% .x)),
-         # add predictions to it
-         target_df_pred = map2(target_df, mod, ~ add_predictions(.x, .y[[1]], "detritus0_NA")),
-         target_df_pred = map(target_df_pred, ~ .x %>% mutate(detritus0_NA = exp(detritus0_NA)))
-  ) %>%
-  {list(observed = .[["src_newv"]],
-        predicted = .[["target_df_pred"]]
-        )}
-
-df_list %>%
-  flatten %>%
-  bind_rows(.id = "detritus0_NA_pred") %>%
-  ggplot(aes(x = diameter, y = detritus0_NA, colour = detritus0_NA_pred)) +
-  geom_point() +
-  coord_trans(y = "log")
+#
+# ## for validating
+#
+# list(rmse, rsquare, mae) %>%
+#   map(invoke_rows, .d = test_mod %>% select(m_id, data = src_df, model = predicting_model), .collate = "cols")
+# # TODO extract these into a data_frame
+#
+# list(rmse, rsquare, mae) %>%
+#   invoke_map(list(model = test_mod$predicting_model,
+#                   data = test_mod$src_df))
+#
+# # split by target or source data, predict (use same name) then combine -- labels
+# # to new column called something like obs_or_prediction
+#
+# df_list <- test %>%
+#   select(target_dat, src_newv, mod) %>%
+#   # get the targetd df just as above
+#   mutate(target_df = map(target_dat, ~ detritus_wider_correct_frenchguiana %>%
+#                         filter(dataset_id %in% .x)),
+#          # add predictions to it
+#          target_df_pred = map2(target_df, mod, ~ add_predictions(.x, .y[[1]], "detritus0_NA")),
+#          target_df_pred = map(target_df_pred, ~ .x %>% mutate(detritus0_NA = exp(detritus0_NA)))
+#   ) %>%
+#   {list(observed = .[["src_newv"]],
+#         predicted = .[["target_df_pred"]]
+#         )}
+#
+# df_list %>%
+#   flatten %>%
+#   bind_rows(.id = "detritus0_NA_pred") %>%
+#   ggplot(aes(x = diameter, y = detritus0_NA, colour = detritus0_NA_pred)) +
+#   geom_point() +
+#   coord_trans(y = "log")
 
 
 ## maybe one huge data.frame is not helpful -- try invoke_rows with smaller, function-specific data_frames that can then be gathered
