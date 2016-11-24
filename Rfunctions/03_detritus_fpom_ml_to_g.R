4ec23feceb51def7d9541e26080b419cfbd4547d
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-11-24 23:47:03 +0100

    create new workflow based on prediction::prediction(). saves time and is much cleaner!

--- Rfunctions/03_detritus_fpom_ml_to_g.R
+++ Rfunctions/03_detritus_fpom_ml_to_g.R
@@ -10,8 +10,8 @@
 read_fpom_fg <- function(path) {
   path %>%
     read_csv(col_types =
-               cols(
-                 .default = col_character(),
+               cols_only(
+                 # .default = col_character(),
                  `FPOM (ml decanted)` = col_double(),
                  `FPOM (mm3)` = col_integer(),
                  `dry weight (g)` = col_double(),
@@ -35,25 +35,15 @@ fit_fpom_g_ml <- function(fpom_fg_data) {
 
 #' convenience function. This takes a model object and returns a function, one
 #' that takes a vector of new x values and returns a list of predictions
-predict_function <- function(mod) {
+get_response_from_model <- function(mod) {
 
   respname <- mod$model %>%
     attr("terms") %>%
     formula() %>%
     all.vars(.) %>%
-    .[2]
+    .[1]
 
-  function(xs) {
-    force(respname)
-    force(mod)
-
-    xs %>%
-      list %>%
-      set_names(respname) %>%
-      predict(mod, se.fit = TRUE, .) %>%
-      ## returns a list. Add in the original values:
-      splice(xs = xs, .)
-  }
+  return(respname)
 }
 
 
@@ -61,91 +51,34 @@ predict_function <- function(mod) {
 #' French Guiana. This uses the convenience function `predict_function()` to
 #' create the workhorse function that does the actual work
 
-estimate_fpom_g_from_ml <- function(.detritus_wider_cardoso_corrected, .model_fpom_g_ml) {
+add_predictions_to_data <- function(.df, .model) {
 
-  ## create function to estimate g from ml
-  fpom_convert_ml_into_g <- predict_function(.model_fpom_g_ml)
+  respname <- get_response_from_model(.model)
 
-  .detritus_wider_cardoso_corrected %>%
-    tbl_df %>%
-    select(dataset_id, bromeliad_id, dataset_name, fpom_ml) %>%
-    nest(bromeliad_id, fpom_ml) %>%
-    ## should there perhaps be a check that i'm not writing over data?
-    mutate(detritus0_150_prediction = map_if(data,
-                                             ## UGH is there a better way to say "wherever that column is not all NA"
-                                             ~ .x$fpom_ml %>% negate(is.na)(.) %>% all,
-                                             ## if it is not all NA -- in other
-                                             ## words there are some values --
-                                             ## then run the prediction function
-                                             ## and spit out a list that contains the ids of the prediction
-                                             ~ splice(bromeliad_id = .x$bromeliad_id,
-                                                      fpom_convert_ml_into_g(.x$fpom_ml))))
+  # get predictions
+  model_predictions <- prediction(data = .df, model = .model)
 
-  ## here it could also spit out a message
+  # rename predictions
+  names(model_predictions) <- paste(respname, names(model_predictions), sep = "_")
 
+  # export list of prediciton object -- just in case i want it later! and also
+  # the colbound data.fram
+  outlist <- list(model_predictions = model_predictions,
+                  data_with_prediction = bind_cols(.df, model_predictions)
+  )
+
+  return(outlist)
 }
 
-## this function takes a data.frame that has a mix of observed data and
-## prediction objects in the same list-column. this column is "heterogeneous" --
-## some elements are lists, others data.frames
-predict_fpom_g <- function(.detritus_wider_FG_g) {
-
-  ## convenience function for creating a data.frame out fo the predictions and standard errors
-  tidy_prediction <- function(pred_list){
-    data_frame(detritus0_150_pv = pred_list$fit,
-               detritus0_150_se = pred_list$se.fit)
-  }
-
-  det_pred <- .detritus_wider_FG_g %>%
-    mutate(detritus0_150_just_brom = map(detritus0_150_prediction,
-                                         ## everyone should have "bromeliad_id"
-                                         ~ data_frame(bromeliad_id = .x$bromeliad_id)),
-           detritus0_150_just_pred = map(detritus0_150_prediction,
-                                         ## if you don't have prediction variables, u are not a prediction
-                                         possibly(tidy_prediction, data_frame(detritus0_150_pv = NA_real_,
-                                                                              detritus0_150_se = NA_real_))),
-           ## stick em together (use cbind for the recycling)
-           detritus0_150_df        = map2(detritus0_150_just_brom, detritus0_150_just_pred, cbind))
-
-  ## This approach with `safely` is OK. it would be even simpler with
-  ## `possibly`, but what if we need those errors someday?? they would go here.
-
-  det_pred %>%
-    unnest(detritus0_150_df)
-}
-##
-
-
-## nest the list where there is the same dataset id and then, then, you should
-## do the function wherever there is data. Thin ein a separate step, why don't
 ## you just move the predictions (and the SEs) over where there is NOT data?
 
 
 ## put the predicted values with the originals. Combine them side-by-side and
 ## then run simple check -- there should be no predicted detritus where there is
 ## also observed detritus
-combine_observed_predicted_0_150_det <- function(.detritus_wider_correct_frenchguiana, .detritus_wider_fpom_g_predicted) {
+combine_observed_predicted_0_150_det <- function(.detritus_wider_fpom_g_pred) {
 
-# browser()
-  combined_cols <- .detritus_wider_correct_frenchguiana %>%
-    # rename for clarity
-    rename(detritus0_150_orig = detritus0_150) %>%
-    left_join(.detritus_wider_fpom_g_predicted, by = c("dataset_id", "dataset_name", "bromeliad_id")) %>%
-    # there should be no value present in both columns -- this is a horrible way to do it!
-    verify(rowSums(cbind(!is.na(detritus0_150_orig), !is.na(detritus0_150_pv))) < 2) %>%
-    # combine the two columns -- using ifelse should now be safe if the above check passed :D
-    mutate(detritus0_150 = if_else(is.na(detritus0_150_orig), detritus0_150_pv, detritus0_150_orig))
-
-  # then check with (either) of the fpom columns
-  combined_cols %>%
-    check_data_source_target("fpom_ml", "detritus0_150")
-
-  # then clean it out if it works
-  combined_cols %>%
-    select(-fpom_ml, -detritus0_150_orig, -detritus0_150_pv)
-  ## note that we keep detritus0_150_se which is now useful for prediction etc
-  ## etc, could also be used to identify predicted values and reg values.
-
+  .detritus_wider_fpom_g_pred[["data_with_prediction"]]
 }
 
 ## OK an implicit assumption in the above is that all sites can and should
