dc9435ac5919c49a3a30f36cba74f26d73ca859f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2016-12-03 11:11:08 +0100

    wrap it all in a big ugly function -- then unsuccessfully apply it to data

--- Rfunctions/22_detritus_combination.R
+++ Rfunctions/22_detritus_combination.R
@@ -3,93 +3,111 @@
 # to say if the answer was observed, or partly predicted, and over what exact
 # range the predictions apply
 
+summarize_detritus <- function(det_df){
+  detritus_category_df <- det_df %>%
+    tbl_df() %>%
+    # just the detritus
+    select(bromeliad_id, starts_with("detritus")) %>%
+    # remove any categories which are all NA
+    # keep(~ negate(all)(is.na(.x))) # that's one option but I think its weak
+    gather(detritus_category, detritus_amount, starts_with("detritus")) %>%
+    group_by(detritus_category) %>%
+    nest
 
-detritus_category_df <- detritus_estimated_with_model$pred_data[[2]] %>%
-  tbl_df() %>%
-  # just the detritus
-  select(bromeliad_id, starts_with("detritus")) %>%
-  # remove any categories which are all NA
-  # keep(~ negate(all)(is.na(.x))) # that's one option but I think its weak
-  gather(detritus_category, detritus_amount, starts_with("detritus")) %>%
-  group_by(detritus_category) %>%
-  nest
 
 
+  # drop the ones that are all empty
+  categories_ses <- detritus_category_df %>%
+    # need to get rid of the detritus0_150 and detritus0_150_combo columns -- deal
+    # with this earlier? if the SE here is used as a weight, then that will
+    # "filter through" to later estimates and be caught below
+    filter(!detritus_category %in% c("detritus0_150", "detritus0_150_src")) %>%
+    mutate(is_empty = map_lgl(data, ~ all(is.na(.x$detritus_amount)))) %>%
+    filter(!is_empty) %>%
+    select(-is_empty)
 
-# drop the ones that are all empty
-categories_ses <- detritus_category_df %>%
-  # need to get rid of the detritus0_150 and detritus0_150_combo columns -- deal
-  # with this earlier? if the SE here is used as a weight, then that will
-  # "filter through" to later estimates and be caught below
-  filter(!detritus_category %in% c("detritus0_150", "detritus0_150_src")) %>%
-  mutate(is_empty = map_lgl(data, ~ all(is.na(.x$detritus_amount)))) %>%
-  filter(!is_empty) %>%
-  select(-is_empty)
+  # we need to split up _fitted values (to get the range) but I want to leave the
+  # _se.fitted alone. so just filter out the _se
 
-# we need to split up _fitted values (to get the range) but I want to leave the
-# _se.fitted alone. so just filter out the _se
+  fitted_and_obs_vals <- categories_ses %>%
+    filter(!str_detect(detritus_category, "_se.fitted$"))
 
-fitted_and_obs_vals <- categories_ses %>%
-  filter(!str_detect(detritus_category, "_se.fitted$"))
+  # get the unfiltered things
+  categories_ses %>% anti_join(fitted_and_obs_vals, by = "detritus_category")
 
-# get the unfiltered things
-categories_ses %>% anti_join(fitted_and_obs_vals, by = "detritus_category")
+  get_det_vals <- fitted_and_obs_vals %>%
+    mutate(obs_or_fit = if_else(str_detect(detritus_category, "_fitted"), true = "fit", false = "obs")) %>%
+    mutate(detritus_broken_up = str_split(detritus_category, "_"))
 
-get_det_vals <- fitted_and_obs_vals %>%
-  mutate(obs_or_fit = if_else(str_detect(detritus_category, "_fitted"), true = "fit", false = "obs")) %>%
-  mutate(detritus_broken_up = str_split(detritus_category, "_"))
+  find_range <- function(x){
+    # NA at the end of a string (or near the end) indicates that that sieve
+    # included detritus up to the biggest size present
+    is_NA_present <- any(str_detect(x, "NA"))
+    # use readr functions to parse it to numbers
 
-find_range <- function(x){
-  # NA at the end of a string (or near the end) indicates that that sieve
-  # included detritus up to the biggest size present
-  is_NA_present <- any(str_detect(x, "NA"))
-  # use readr functions to parse it to numbers
+    numeric_detritus <- parse_number(x)
 
-  numeric_detritus <- parse_number(x)
+    if(is_NA_present){
+      maxval <- Inf
+    } else {
+      maxval <- max(numeric_detritus, na.rm = TRUE)
+    }
 
-  if(is_NA_present){
-    maxval <- Inf
-  } else {
-    maxval <- max(numeric_detritus, na.rm = TRUE)
+    minval <- min(numeric_detritus, na.rm = TRUE)
+
+    data.frame(min_detritus = minval, max_detritus = maxval)
   }
 
-  minval <- min(numeric_detritus, na.rm = TRUE)
+  detritus_total <- get_det_vals %>%
+    mutate(detritus_max = map(detritus_broken_up, find_range)) %>%
+    unnest(detritus_max) %>%
+    # tests here?
+    # can ditch the broken_up col
+    select(-detritus_broken_up)
 
-  data.frame(min_detritus = minval, max_detritus = maxval)
+  # test if they are consecutive
+
+  consec_test_vec <- detritus_total %>%
+    # by using lead we can be cunning: skipping the final maximum size (which may be Inf)
+    mutate(is_consec = lead(min_detritus) == max_detritus) %>%
+    .[["is_consec"]]
+  # should all be true except the last (which is NA)
+  all(consec_test_vec[-length(consec_test_vec)])
+
+  # if that passed, add em together.There are two things to add -- the column "obs_or_fit" and the data itself
+  det_by_bromeliad <- detritus_total %>%
+    select(-detritus_category) %>%
+    unnest(data) %>%
+    group_by(bromeliad_id) %>%
+    nest
+
+  det_by_bromeliad$data[[1]]
+
+  det_by_bromeliad %>%
+    mutate(summary_df = map(data, ~data_frame(min_det = min(.x$min_detritus),
+                                              max_det = max(.x$max_detritus),
+                                              obs_or_fit = paste0(.x$obs_or_fit, collapse = "_"),
+                                              total_detritus = sum(as.numeric(.x$detritus_amount))))) %>%
+    unnest(summary_df) %>%
+    select(-data)
 }
 
-detritus_total <- get_det_vals %>%
-  mutate(detritus_max = map(detritus_broken_up, find_range)) %>%
-  unnest(detritus_max) %>%
-  # tests here?
-  # can ditch the broken_up col
-  select(-detritus_broken_up)
+detritus_total_model <- detritus_estimated_with_model %>%
+  mutate(det_sum = map(pred_data, summarize_detritus)) %>%
+  select(det_sum) %>%
+  unnest
 
-# test if they are consecutive
+detritus_total_model
 
-consec_test_vec <- detritus_total %>%
-  # by using lead we can be cunning: skipping the final maximum size (which may be Inf)
-  mutate(is_consec = lead(min_detritus) == max_detritus) %>%
-  .[["is_consec"]]
-# should all be true except the last (which is NA)
-all(consec_test_vec[-length(consec_test_vec)])
 
-# if that passed, add em together.There are two things to add -- the column "obs_or_fit" and the data itself
-det_by_bromeliad <- detritus_total %>%
-  select(-detritus_category) %>%
-  unnest(data) %>%
-  group_by(bromeliad_id) %>%
-  nest
+detritus_total_obs <- detritus_wider_new_variables %>%
+  anti_join(detritus_total_model, by = "bromeliad_id") %>%
+  group_by(dataset_id, visit_id) %>%
+  nest %>%
+  mutate(det_sum = map(data, summarize_detritus))
 
-det_by_bromeliad$data[[1]]
 
-det_by_bromeliad %>%
-  mutate(summary_df = map(data, ~data_frame(min_det = min(.x$min_detritus),
-                                           max_det = max(.x$max_detritus),
-                                           obs_or_fit = paste0(.x$obs_or_fit, collapse = "_"),
-                                           total_detritus = sum(as.numeric(.x$detritus_amount))))) %>%
-  unnest(summary_df)
-
+detritus_total_obs %>% View
 
 # here join on the se column, (what if there is two?). also can rename or drop detritus_category
 
