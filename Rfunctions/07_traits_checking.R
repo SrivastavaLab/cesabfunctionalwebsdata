2a2a6b64ff4ccff5ba76aabe2a219654ee71cc9d
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2017-01-11 11:15:01 +0100

    API changes (and perhaps also changes to the underlying database?) make this checking code useless!

--- Rfunctions/07_traits_checking.R
+++ Rfunctions/07_traits_checking.R
@@ -3,32 +3,12 @@
 ### letter and N a number) and in which all traits are unique
 
 rename_check_traits <- function(.trts_all_filtered){
-  ## in order to find out where the traits between the different columsn are
-  ## similar, we spread the two categories ("traits" and "tachet").We then stick
-  ## them together.
-  traits_all_combined <- .trts_all_filtered %>%
-    gather(trtname, value, starts_with("tachet"), starts_with("traits")) %>%
-    separate(trtname, into = c("tach_trait", "traitmode")) %>%
-    spread(tach_trait, value) %>%
-    replace_na(list(tachet = "", traits = "")) %>%
-    mutate(combotrait = paste0(tachet, traits))
+  ## drop the "tachet"
+  names(.trts_all_filtered) <- str_replace(names(.trts_all_filtered), "tachet\\.", "")
 
-  ## check for problems -- if there is more than one character in the traits (all
-  ## shold be single digits) then this indicates that the two tables had different
-  ## values
-  traits_all_combined %>%
-    filter(nchar(combotrait) > 1) %>%
-    assertr::verify(nrow(.) == 0)
-
-  ## if there are no problems -- convert the combined trait column to numeric,
-  ## delete all, make traits columns.
-  traits_all_combined %>%
-    mutate(combotrait2 = as.numeric(combotrait)) %>%
-    select(-tachet, -traits, -combotrait) %>%
-    spread(traitmode, combotrait2) %>%
-    assertr::verify(nrow(.) == nrow(rename_check_traits)) %>%
-    # refining output by making "NA" into a true NA
-    mutate_if(is.character, readr::parse_character)
+  # check all the character columns and make them numbers where possible. This
+  # should be the case for all traits of the form XXN
+  mutate_if(.trts_all_filtered, is.character, readr::parse_guess)
 }
 
 
