04595ed097c9d9e1f2ede7ea26d890461e47757b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2017-07-07 10:19:40 -0300

    new code to add the guessed-at volume requirements. closes #14

--- Rfunctions/24_volume_imputation.R
+++ Rfunctions/24_volume_imputation.R
@@ -77,11 +77,14 @@ predict_add_imputed <- function(.supp_size_model_fits, .bromeliad_detritus) {
     mutate(predicted_size = map2(data, predicting_model, add_predictions, var = "predicted_water"))
 
   visit_and_predictions <- predicted_max_volume %>%
-    unnest(predicted_size)
+    unnest(predicted_size) %>%
+    select(visit_id, bromeliad_id, predicted_water)
 
   # finally combine with the final thing
 
   observed_and_guesses <- .bromeliad_detritus %>%
+    # select the maximum volume
+    select(visit_id, bromeliad_id, max_water) %>%
     # remove sites which were not imputed
     anti_join(visit_and_predictions, by = "visit_id") %>%
     # then stick the new values on the bottom (this should fill in NA for the new volume things)
@@ -96,13 +99,91 @@ predict_add_imputed <- function(.supp_size_model_fits, .bromeliad_detritus) {
     {stopifnot(nrow(.) == 0)}
 
 
-  observed_and_guesses %>%
+  # finally combine with the final thing
+
+  new_water <- observed_and_guesses %>%
     mutate(max_water_combined = if_else(!is.na(predicted_water), predicted_water, max_water),
-           max_water_imputed = if_else(!is.na(predicted_water), "imputed", "observed")) %>%
-    # can drop predicted size
-    select(-predicted_water)
+           max_water_estimated = if_else(!is.na(predicted_water), "estimated", "observed")) %>%
+    # can drop predicted size -- but let's keep it anyhow
+    # select(predicted_water) %>%
+    # drop max water because it is in the original data
+    select(-max_water)
+
+  .bromeliad_detritus %>%
+    left_join(new_water, by = c("visit_id", "bromeliad_id"))
+
 }
 
-# This last bit of code supplies the workflow for all the rest
+# This last bit of code supplies the workflow that can be adapted to the next
+# thing too -- the recorded, estimated water values
+
+read_volume_estimated <- function(fname) {
+  read_csv(fname, col_types = cols(
+    bromeliad_id = col_character(),
+    max_water = col_double()
+  ))
+}
+
+# There's a problem in here!! produces NA visits -- probably because visit was
+# not in the "added in data"
+
+add_24_volum_data <- function(.volume_estimated, .bromeliad_detritus_vol_imputed) {
+
+  # prepare for merging -- take only the non-NA values
+  .volume_estimated_values <- .volume_estimated %>%
+    filter(!is.na(max_water)) %>%
+    rename(max_water_from_24_volume = max_water)
+
+  # follow this workflow
+
+  # replace .bromeliad_detritus with whatever
+  observed_and_guesses <- .bromeliad_detritus_vol_imputed %>%
+    # working right now only with volume measurements --
+    # use "combined" max water, which contains a few values from the body.
+    # max_water_estimated is here for bookkeeping.
+    select(bromeliad_id, max_water_combined, max_water_estimated) %>%
+    # remove sites which were not estimated
+    anti_join(.volume_estimated_values, by = "bromeliad_id") %>%
+    # then stick the new values on the bottom (this should fill in NA for the new volume things)
+    bind_rows(.volume_estimated_values)
+
+  # a little check to make sure everything is OK
+  stopifnot(nrow(observed_and_guesses) == nrow(.bromeliad_detritus_vol_imputed))
+
+  # it should be impossible to predict an observed number! It should only have
+  # been done where observations are absent
+  observed_and_guesses %>%
+    filter(!is.na(max_water_combined) & !is.na(max_water_from_24_volume)) %>%
+    {stopifnot(nrow(.) == 0)}
 
 
+  new_values <- observed_and_guesses %>%
+    mutate(max_water_combined = if_else(!is.na(max_water_from_24_volume),
+                                        max_water_from_24_volume,
+                                        max_water_combined),
+           max_water_estimated_2 = if_else(!is.na(max_water_from_24_volume),
+                                         "estimated",
+                                         max_water_estimated)) #%>%
+    # can drop predicted size
+    # select(-max_water_from_24_volume)
+
+  # browser()
+
+  # take only the bromeliad_id and the new water (max_water_combined) and the new estimated column
+
+  # need to clear this up by not duplicating columns
+  old_data_minus_intermediate_error <- .bromeliad_detritus_vol_imputed %>%
+    select(-max_water_combined, -max_water_estimated)
+
+
+  combined_water <- old_data_minus_intermediate_error %>%
+    left_join(new_values, by = "bromeliad_id")
+
+
+  with(combined_water, table(max_water_estimated, max_water_estimated_2))
+
+  combined_water %>%
+    select(-max_water_estimated) %>%
+    rename(max_water_estimated = max_water_estimated_2)
+
+}
