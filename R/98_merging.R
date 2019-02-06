43a8b6f130bb974511c8daa3093601b9877eceed
Alathea <alathea@lutrisanalytics.com>
2019-02-06 11:51:16 -0800

    retrieve the bwgbiomass data and as a final data.frame in the list

--- R/98_merging.R
+++ R/98_merging.R
@@ -11,6 +11,11 @@ traits <-   read_csv("data-raw/13_T_matrix.csv")
 bromeliads <- read.csv("data-raw/29_full_bromeliad_wide.csv", stringsAsFactors = FALSE)
 abundance <- read_csv("data-raw/51_abundance.csv")
 wbe <- readRDS("data-raw/81_WBE.RDS")
+## This retrieves the biomass data from "bwgbiomass" and adds it as a final
+## data.frame in the list
+biomass <- datastorr::github_release_get(
+  datastorr::github_release_info("SrivastavaLab/bwgbiomass", read = readRDS)
+  )
 
 all_data_list <- list(
   datasets   = datasets,
@@ -18,6 +23,7 @@ all_data_list <- list(
   traits     = traits,
   bromeliads = bromeliads,
   abundance  = abundance,
-  WBE        = wbe)
+  WBE        = wbe
+  biomass    = biomass)
 
 saveRDS(all_data_list, "~/Desktop/all_data.rds")
