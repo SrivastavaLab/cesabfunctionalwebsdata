c9b9e8fcafa6f43f4971b2ab37a9830cf17c5a67
Alathea <alathea@lutrisanalytics.com>
2019-02-06 12:04:59 -0800

    added biomass data frame as list item in final release

--- Rfunctions/RELEASE.R
+++ Rfunctions/RELEASE.R
@@ -2,6 +2,12 @@ create_data_release <- function(filename, datasets, visits, traits,
                                 bromeliads, abundance,
                                 synonymous_names, abundance_matrix){
 
+  ## This retrieves the biomass data from "bwgbiomass" and adds it as a final
+  ## data.frame in the list
+  biomass <- datastorr::github_release_get(
+    datastorr::github_release_info("SrivastavaLab/bwgbiomass", read = readRDS)
+  )
+
   all_data_list <- list(
     datasets   = datasets,
     visits     = visits,
@@ -9,7 +15,8 @@ create_data_release <- function(filename, datasets, visits, traits,
     bromeliads = bromeliads,
     abundance  = abundance,
     synonymous_names = synonymous_names,
-    abundance_matrix = abundance_matrix)
+    abundance_matrix = abundance_matrix,
+    biomass = biomass)
 
   saveRDS(all_data_list, filename)
 }
