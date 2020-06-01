67266153411fe9575908a22606db9e8e4a35b0ff
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-06-01 10:40:34 -0400

    cardoso csv

--- accessing_raw_abds.R
+++ accessing_raw_abds.R
@@ -1,7 +1,7 @@
-dats <- bwg_get("datasets")
-abds <- get_all_abundances(dats)
-
+library(bwgdata) # works with version 0.2.1 ONLY
 library(tidyverse)
+
+dats <- bwg_get("datasets")
 
 dats %>%
   filter(location == "Cardoso")
@@ -12,54 +12,32 @@ cardoso_raw <- bwg_get("matrix", opts = list(dataset_id = 6))
 
 str(cardoso_raw, max.level = 2)
 
-map_df(cardoso_raw[[1]], 1, .id = "species_id")
-
-cardoso_raw[[1]][[1]]$measurements
-
-# Diptera.190
-
+## need to extract first element of list, then pull out the first and second
+## elements of the list associated with each species. This corresponds to the
+## bwgname and the measurement of that species
 cardoso_meas <- cardoso_raw[[1]] %>%
-  enframe %>%
+  enframe(name = "species.key") %>%
   mutate(bwgname = map_chr(value, 1),
-         measure = map(value,2))
-
-cardoso_meas %>%
-  filter(bwgname == "Diptera.190") %>% .[["measure"]] %>% .[[1]] %>% enframe %>%
-  mutate(cat = map_chr(value, "category_range"),
-         mes = map(value, "measurement"))
-
-cardoso_almost_flat <- cardoso_meas %>%
-  filter(bwgname == "Diptera.193") %>%
-  .[["measure"]] %>% .[[1]] %>% enframe %>%
-  mutate(cat = map_chr(value, "category_range"),
-         mes = map_chr(value, c("measurement", "value")),
-         abd = map(value, 3))
-#
-# cardoso_almost_flat$abd[[1]] %>%
-#   enframe(name = "bromeliad_id", value = "abd") %>%
-#   unnest(cols = c(abd))
-
-make_brom_df <- . %>%
-  enframe(name = "bromeliad_id", value = "abd") %>%
-  unnest(cols = c(abd))
-
-cardoso_almost_flat %>%
-  mutate(abd_brom = map(abd, make_brom_df)) %>%
-  select(-value, -abd) %>% unnest(abd_brom)
+         measure = map(value,2)) %>%
+  select(-value)
 
 
 cardoso_almost_flat <- cardoso_meas %>%
+  # measure is a nested list; extract and convert to a dataframe
   mutate(meas_list = map(measure, 1),
          meas_df = map(meas_list, enframe, name = "listcontents")) %>%
-  select(-value, -measure, -meas_list) %>%
+  select(-measure, -meas_list) %>%
+  # unpack then pivot the contents to columns
   unnest(meas_df) %>%
   pivot_wider(names_from = "listcontents", values_from = "value")
 
-
+## flatten and unpack the columns
 cardoso_almost_flat %>%
   mutate(category_range = flatten_chr(category_range),
          measurement = map_chr(measurement, "value"),
          bromeliads = map(bromeliads, enframe)) %>%
-  rename(species.key = name) %>%
   unnest(bromeliads) %>%
-  mutate(value = flatten_chr(value))
+  mutate(value = flatten_chr(value) %>% readr::parse_double(.)) %>%
+  write_csv("cardoso_abundances_and_measurements.csv")
+
+
